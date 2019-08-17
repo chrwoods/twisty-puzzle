fc2f96496c0cdc14692de65f96dc9f001d08307f
chrwoods <chrisscottwoods@gmail.com>
2019-08-17 15:44:28 -0500

    Started writing support for display mapping

--- cube.h
+++ cube.h
@@ -1,8 +1,8 @@
 typedef struct cube {
-  short front[SIZE][SIZE];
   short up[SIZE][SIZE];
-  short right[SIZE][SIZE];
-  short down[SIZE][SIZE];
   short left[SIZE][SIZE];
+  short front[SIZE][SIZE];
+  short right[SIZE][SIZE];
   short back[SIZE][SIZE];
+  short down[SIZE][SIZE];
 } cube;
--- turn.c
+++ turn.c
@@ -1,8 +1,12 @@
-void rotate_face(short face[SIZE][SIZE]) {
+void rotate_face(short face[SIZE][SIZE], short clockwise) {
   short rotated[SIZE][SIZE];
   for(int i = 0; i < SIZE; i++) {
     for(int j = 0; j < SIZE; j++) {
-      rotated[j][SIZE - 1 - i] = face[i][j];
+      if(clockwise) {
+	rotated[j][SIZE - 1 - i] = face[i][j];
+      } else {
+	rotated[i][j] = face[j][SIZE - 1 - i];
+      }
     }
   }
   for(int i = 0; i < SIZE; i++) {
